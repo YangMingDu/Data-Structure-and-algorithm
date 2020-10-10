a2f672d31fde011872cac38b21262c9de304294b
YangMing Du <duyangming84830727@gmail.com>
2020-10-10 21:11:08 +0200

    First commit

---
+++ TimeComplexitOnlogn/Application of merge algorithm/InsertionSort.h
@@ -0,0 +1,33 @@
+#ifndef MERGESORTOPTIMIZED_INSERTIONSORT_H
+#define MERGESORTOPTIMIZED_INSERTIONSORT_H
+
+
+template<typename T>
+void InsertionSort(T arr[],int left,int right){
+
+    for(int i = left + 1; i <= right; i++){
+        T e = arr[i];
+        int j;
+        for(j = i; j > left  && arr[j-1] > e; j--){
+             arr[j] = arr[j-1]; /*找到合适的位置j，在找到合适位置之前将不合适的元素向后移*/
+        }
+        arr[j] = e; /*找到arr[i]的合适位置，并将arr[j]赋值*/ 
+    }
+ 
+}
+
+template<typename T>
+void InsertionSort(T arr[],int n){
+
+    for(int i = 1; i < n; i++){
+        T e = arr[i];
+        int j;
+        for(j = i; j > 0  && arr[j-1] > e; j--){
+             arr[j] = arr[j-1]; /*找到合适的位置j，在找到合适位置之前将不合适的元素向后移*/
+        }
+        arr[j] = e; /*找到arr[i]的合适位置，并将arr[j]赋值*/ 
+    }
+ 
+}
+
+#endif
