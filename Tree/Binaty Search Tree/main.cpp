8a25399a7fcb406fb99b957b004848c5189feec7
YangMing Du <duyangming84830727@gmail.com>
2020-10-14 23:34:13 +0200

    First commit

--- Tree/Binaty Search Tree/main.cpp
+++ Tree/Binaty Search Tree/main.cpp
@@ -40,10 +40,21 @@ public:
     bool isEmpty(){
         return count == 0;
     }
+    //插入（递归）
     void insert(Key key,Value value){
 
         root = insert(root,key,value);
     }
+    
+    void contain(Key key){
+
+        return contain(root,key);
+    }
+    //如果返回Node*，则需要把Node结构体放到public上，但是这样封装不是很好，如果返回Value的话，value不能为空，也就是说用户必须先contain再search，这次返回为value*可以为空，也方便用户更改数据。
+    Value* search(Key key){
+        return search(root,key);
+    }
+
 
 private:
     //向以node为根的二叉搜索树中插入节点（key，value）
@@ -65,6 +76,42 @@ private:
         }
 
         return node;
+    }
+
+    //在以node为根的二叉搜索树中是否包含键值为key的节点
+    bool contain(Node* node,Key key){
+
+        if(node == NULL){
+            return false;
+        }
+
+        if(key == Node->key){
+            return true;
+        }
+        else if(key < Node->key){
+            return contain(node->left,key);
+        }
+        else{
+            return contain(node->right,key);
+        }
+
+
+    }
+    //在以node为根的二叉搜索树中查找key所对应的value。
+    Value* search(Node* node,Key key){
+        if(node == NULL){
+            return false;
+        }
+
+        if(key == Node->key){
+            return &(node->value);//Value的地址
+        }
+        else if(key < Node->key){
+            return search(node->left,key);
+        }
+        else{
+            return search(node->right,key);
+        }
     }
 
 
