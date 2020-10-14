b838f564e4c44e45876e7c36bdf549b080845766
YangMing Du <duyangming84830727@gmail.com>
2020-10-14 23:06:29 +0200

    First commit

--- Tree/Binaty Search Tree/main.cpp
+++ Tree/Binaty Search Tree/main.cpp
@@ -40,12 +40,38 @@ public:
     bool isEmpty(){
         return count == 0;
     }
+    void insert(Key key,Value value){
+
+        root = insert(root,key,value);
+    }
+
+private:
+    //向以node为根的二叉搜索树中插入节点（key，value）
+    //返回新插入节点后的二叉搜索树的根
+    Node* insert(Node *node, Key key, Value value){
+
+        if( node == NULL ){
+            count ++;
+            return new Node(key,value);
+        }
+        if( key == node->key){
+            node -> value = value;
+        }
+        else if(key < node->key){
+            node -> left = insert(node->left, key, value);
+        }
+        else{
+            node -> right = insert(node->right, key, value);
+        }
+
+        return node;
+    }
 
 
 };
 
 int main(){
 
-    
+
     return 0;
 }
