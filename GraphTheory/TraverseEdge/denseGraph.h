a4ef82770a34fc01a768f87329bb6de3c638197a
YangMing Du <duyangming84830727@gmail.com>
2020-10-19 23:26:25 +0200

    First commit

---
+++ GraphTheory/TraverseEdge/denseGraph.h
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+using namespace ::std;
+
+class denseGraph{
+//稠密图，用邻接矩阵实现
+private:
+
+    int n,m;//n个点，m条边
+    bool directed;//判断有向图还是无向图
+    vector<vector<bool>> g;//二维数组，两点之间是否有边
+
+public:
+
+    denseGraph( int n, bool directed){
+        this->n = n;
+        this->m = 0;
+        this->directed = directed;
+        for(int i = 0; i < n; i ++){
+            g.push_back(vector<bool>(n,false));
+        } 
+    }
+
+    ~denseGraph(){
+
+    }
+
+    int V(){return n;}
+    int E(){return m;}
+
+    void addEdge( int v, int w){
+        assert( v >= 0 && v < n);
+        assert( w >= 0 && w < n);
+        //如果已经有边，直接返回
+        if(hasEdge(v,w)){
+            return;
+        }
+        g[v][w] = true;
+        if( !directed ){
+            g[w][v] = true;
+        }
+
+
+        m ++;
+    }
+    //判断是否已经有边
+    bool hasEdge (int v, int w){
+        assert( v >= 0 && v < n);
+        assert( w >= 0 && w < n); 
+        return g[v][w];      
+    }
+
+
+};
---
+++ GraphTheory/TraverseEdge/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <ctime>
+#include "sparseGraph.h"
+
+
+using namespace :: std;
+int main(){
+    
+    
+    int M = 20;
+    int N = 100;
+
+    srand( time(NULL) );
+    //生成20个点100条边的图
+    spareGraph g1(N,false);
+    for (int i = 0 ; i < M ; i ++){
+        int a = rand()%N;
+        int b = rand()%N;
+        //连接边
+        g1.addEdge( a , b);
+
+    }
+    for( int v = 0 ; v < N; v ++){
+        cout << v << " ";
+        spareGraph::adjIterator adj( g1 , v);
+        for( int w  = adj.begin() ; !adj.end() ; w = adj.next() ){
+            cout<<w<<" ";
+        }
+        cout<<endl;
+    }
+
+}
