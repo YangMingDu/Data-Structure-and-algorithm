43b05f491e497446ffdeba5220313140369da606
YangMing Du <duyangming84830727@gmail.com>
2020-10-27 22:39:39 +0100

    First commit

--- LinkedList/Build_ a_linkedlist/main.cpp
+++ LinkedList/Build_ a_linkedlist/main.cpp
@@ -81,15 +81,42 @@ class Solution {
 
 
 };
+class Solution2 {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* head1 = new ListNode(-1); // 记录比x小的节点组成的链表
+        ListNode* head2 = new ListNode(-1); // 记录比x大的节点组成的链表
+        ListNode* cur1 = head1;
+        ListNode* cur2 = head2;
+        while (head) {
+            if (head -> val < x) {
+                cur1 -> next = head;
+                cur1 = cur1 -> next;
+            } else {
+                cur2 -> next = head;
+                cur2 = cur2 -> next;
+            }
+            head = head -> next;
+        }
+        // 将两个链表拼接起来
+        cur2 -> next = nullptr;
+        cur1 -> next = head2 -> next;
+
+        ListNode *ret = head1 -> next;
+        delete head1;
+        delete head2;
+        return ret;
+    }
+};
 
 int main(){
-    int arr[] = {1,2,3,4,5,6};
+    int arr[] = {1,1};
     int n = sizeof(arr)/sizeof(int);
 
     ListNode* head = createLinkedList(arr,n);
     printLinkedList(head);
 
-    ListNode* head2 = Solution().reverseBetween(head,2,4);
+    ListNode* head2 = Solution2().partition(head,0);
 
     printLinkedList(head2);
 
