1fcf73e73aa124aae71cfab5a653a98e8cffae46
YangMing Du <duyangming84830727@gmail.com>
2020-10-27 22:51:21 +0100

    First commit

---
+++ Leetcode/Linked List/328-Odd Even Linked List/main.cpp
@@ -0,0 +1,34 @@
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(nullptr) {}
+  };
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* head1 = new ListNode(-1); // 记录奇数的节点组成的链表
+        ListNode* head2 = new ListNode(-1); // 记录偶数的节点组成的链表
+        ListNode* cur1 = head1;
+        ListNode* cur2 = head2;
+        int count = 0;
+        while (head) {
+            count ++;
+            if (count % 2 != 0) {
+                cur1 -> next = head;
+                cur1 = cur1 -> next;
+            } else {
+                cur2 -> next = head;
+                cur2 = cur2 -> next;
+            }
+            head = head -> next;
+        }
+        // 将两个链表拼接起来
+        cur2 -> next = nullptr;
+        cur1 -> next = head2 -> next;
+
+        ListNode *ret = head1 -> next;
+        delete head1;
+        delete head2;
+        return ret;
+    }
+};
