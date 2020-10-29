514f2526b9daa66e68662449b92e63aea94328cf
YangMing Du <duyangming84830727@gmail.com>
2020-10-29 19:20:43 +0100

    First commit

--- Leetcode/Stack and Queue/20-Valid Parentheses/main.cpp
+++ Leetcode/Stack and Queue/20-Valid Parentheses/main.cpp
@@ -9,20 +9,23 @@ public:
     bool isValid(string s) {
 
         stack<char> stack;
-
+        //将左括号压入栈
         for(int i = 0 ; i < s.size() ; i ++){
             if(s[i] == '(' || s[i] == '{' || s[i] == '['){
                 stack.push(s[i]);
             }
+            //出现右括号时
             else{
+                //栈中没有元素，说明没有与之对应的左括号
                 if(stack.size() == 0){
                     return false;
                 }
+                //记录栈顶元素并弹出
                 char c = stack.top();
                 stack.pop();
 
                 char match;
-
+                //查看是否匹配
                 if(s[i] == ')'){
                     match = '(';
                 }
@@ -40,7 +43,7 @@ public:
 
             }
         }
-
+        //如果栈内还有元素，说明有左括号没有与之对应的右括号。
         if(stack.size() != 0){
             return false;
         }
