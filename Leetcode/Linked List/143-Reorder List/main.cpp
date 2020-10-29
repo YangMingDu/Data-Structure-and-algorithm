a1ccdd31ecbfcb130058763e2adcdc60e1de4445
YangMing Du <duyangming84830727@gmail.com>
2020-10-29 17:36:00 +0100

    First commit

--- Leetcode/Linked List/143-Reorder List/main.cpp
+++ Leetcode/Linked List/143-Reorder List/main.cpp
@@ -26,24 +26,25 @@ public:
         if(head == nullptr || head->next == nullptr || head->next->next == nullptr){
             return;
         }
-
+        //快慢指针
         ListNode* slow = head;
         ListNode* fast = head;
-
+        //求出中点节点
         while(fast->next != nullptr && fast->next->next != nullptr){
             fast = fast->next->next;
             slow = slow->next;
         }
 
 
+        //分出后段链表
         ListNode* L2 = slow->next;
         slow->next = nullptr;
-
+        //翻转
         L2 = reverse(L2);
 
         ListNode* L1cur = head;
         ListNode* L2cur = L2;
-
+        //依次插入前一段链表
         while(L1cur != nullptr && L2cur != nullptr){
 
             ListNode* L1next = L1cur->next;
