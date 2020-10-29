dd61b1c5d1535abbb2accf07f593a5b196fe1781
YangMing Du <duyangming84830727@gmail.com>
2020-10-29 19:18:57 +0100

    First commit

---
+++ Leetcode/Stack and Queue/20-Valid Parentheses/main.cpp
@@ -0,0 +1,51 @@
+#include<stack>
+#include<string>
+#include<iostream>
+
+using namespace :: std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> stack;
+
+        for(int i = 0 ; i < s.size() ; i ++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                stack.push(s[i]);
+            }
+            else{
+                if(stack.size() == 0){
+                    return false;
+                }
+                char c = stack.top();
+                stack.pop();
+
+                char match;
+
+                if(s[i] == ')'){
+                    match = '(';
+                }
+                else if(s[i] == ']'){
+                    match = '[';
+                }
+                else{
+                    match = '{';
+                }
+
+                if(c != match){
+                    return false;
+                }
+
+
+            }
+        }
+
+        if(stack.size() != 0){
+            return false;
+        }
+
+        return true;
+
+    }
+};
