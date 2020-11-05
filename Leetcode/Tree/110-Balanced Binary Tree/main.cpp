8299f2542ba4cdaa851ee7852bd046f2223cfe84
YangMing Du <duyangming84830727@gmail.com>
2020-11-05 22:04:37 +0100

    First commit

--- Leetcode/Tree/110-Balanced Binary Tree/main.cpp
+++ Leetcode/Tree/110-Balanced Binary Tree/main.cpp
@@ -10,10 +10,12 @@ using namespace::std;
  };
 class Solution {
 public:
+    //计算任意节点的高度
     int height(TreeNode* root) {
         if (root == nullptr) {
             return 0;
         } else {
+            //任意节点的高度等于他最长的子树（无论左右），再加上自己
             return max(height(root->left), height(root->right)) + 1;
         }
     }
@@ -22,6 +24,7 @@ public:
         if (root == nullptr) {
             return true;
         } else {
+            //相当于遍历所有节点，看看所有节点是不是他们的左子树和右子树的高度差小于等于1
             return abs(height(root->left) - height(root->right)) <= 1 && isBalanced(root->left) && isBalanced(root->right);
         }
     }
