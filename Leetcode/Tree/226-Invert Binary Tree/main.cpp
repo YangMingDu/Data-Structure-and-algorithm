5c3c17a60b8d8776a3862fe95821f3161b399454
YangMing Du <duyangming84830727@gmail.com>
2020-11-05 20:23:17 +0100

    First commit

---
+++ Leetcode/Tree/226-Invert Binary Tree/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace::std;
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+class Solution {
+public:
+
+
+    TreeNode* invertTree(TreeNode* root) {
+
+        if (root == nullptr) {
+            return nullptr;
+        }
+        TreeNode* left = invertTree(root->left);
+        TreeNode* right = invertTree(root->right);
+        //swap(root->left,root->right);
+        root->left = right;
+        root->right = left;
+        return root;
+
+
+    }
+};
