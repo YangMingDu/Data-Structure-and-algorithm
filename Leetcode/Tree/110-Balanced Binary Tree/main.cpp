a58dffba14700495f784613ac76defbcc008304c
YangMing Du <duyangming84830727@gmail.com>
2020-11-05 22:00:18 +0100

    First commit

---
+++ Leetcode/Tree/110-Balanced Binary Tree/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace::std;
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+class Solution {
+public:
+    int height(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        } else {
+            return max(height(root->left), height(root->right)) + 1;
+        }
+    }
+
+    bool isBalanced(TreeNode* root) {
+        if (root == nullptr) {
+            return true;
+        } else {
+            return abs(height(root->left) - height(root->right)) <= 1 && isBalanced(root->left) && isBalanced(root->right);
+        }
+    }
+};
