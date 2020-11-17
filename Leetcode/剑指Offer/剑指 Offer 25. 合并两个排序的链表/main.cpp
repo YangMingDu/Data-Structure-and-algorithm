3671cd372128e205f80653f25279bedaa6756d70
YangMing Du <duyangming84830727@gmail.com>
2020-11-17 22:02:22 +0100

    First commit

---
+++ Leetcode/剑指Offer/剑指 Offer 25. 合并两个排序的链表/main.cpp
@@ -0,0 +1,38 @@
+class Solution {
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+public:
+    //虚拟头
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* dummyhead = new ListNode(-1);
+        ListNode* cur = dummyhead;
+        while(l1 && l2){
+            if(l1->val <= l2->val){
+                cur->next = l1;
+                l1 = l1->next;
+                cur = cur->next;
+            }
+            else{
+                cur->next = l2;
+                l2 = l2->next;
+                cur = cur->next;
+            }
+        }
+
+        if(l1 == nullptr){
+            cur->next = l2;
+        }
+        else if(l2 == nullptr){
+            cur->next = l1;
+        }
+
+        ListNode* ret = dummyhead->next;
+        delete dummyhead;
+        return ret;
+    }
+};
