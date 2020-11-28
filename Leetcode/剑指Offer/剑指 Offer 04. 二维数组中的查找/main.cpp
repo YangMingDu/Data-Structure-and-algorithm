c5d772324ccb46caa53dcd099860a2445ded4630
YangMing Du <duyangming84830727@gmail.com>
2020-11-28 19:59:33 +0100

    First commit

---
+++ Leetcode/剑指Offer/剑指 Offer 04. 二维数组中的查找/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace ::std;
+class Solution {
+public:
+    bool findNumberIn2DArray(vector<vector<int>>& matrix, int target) {
+        if(matrix.size() == 0){
+            return false;
+        }
+        //右上角开始，向左下看是个二叉搜索树
+        int i = 0;
+        int j = matrix[0].size() - 1;
+
+        while(i < matrix.size() && j >= 0){
+            if(matrix[i][j] == target){
+                return true;
+            }
+
+            else if (matrix[i][j] < target){
+                i ++;
+            }
+
+            else{
+                j --;
+            }
+
+
+        }
+
+        return false;
+    }
+};
