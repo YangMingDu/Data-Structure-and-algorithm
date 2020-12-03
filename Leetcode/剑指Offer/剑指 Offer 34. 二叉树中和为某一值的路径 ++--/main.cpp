3f876480f40802a567e1e3cf03595ef53440e10c
YangMing Du <duyangming84830727@gmail.com>
2020-12-03 22:14:35 +0100

    First commit

---
+++ Leetcode/剑指Offer/剑指 Offer 34. 二叉树中和为某一值的路径 ++--/main.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+using namespace :: std;
+struct TreeNode {
+      int val;
+      TreeNode *left;
+     TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> temp;
+
+    void dfs(TreeNode* root,int target){
+        if(root == nullptr){
+            return;
+        }
+        temp.push_back(root->val);
+        target = target - root->val;
+        if(target == 0 && root->left == nullptr && root->right == nullptr){
+            res.push_back(temp);
+        } 
+        else{
+            dfs(root->left,target);
+            dfs(root->right,target);
+        }
+
+        temp.pop_back();
+        target += root->val;
+
+
+    }
+
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+
+        dfs(root,sum);
+        return res;
+    }
+};
